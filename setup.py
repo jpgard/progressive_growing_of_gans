633d27190ab3cb87783215f32169e044e1e69689
jpgard <joshua.patrick.gardner@gmail.com>
2020-10-01 16:19:43 -0700

    create pip-installable python module, `progressive`, for import and use in other modules.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='progressive_growing_of_gans',
+      version='0.1',
+      description='Fork of https://github.com/tkarras/progressive_growing_of_gans',
+      author='Tero Karras (NVIDIA) et al; any modifications in this fork by Josh Gardner',
+      author_email='jpgard@cs.washington.edu',
+      url='https://github.com/jpgard/progressive_growing_of_gans',
+      packages=['progressive'],
+     )
